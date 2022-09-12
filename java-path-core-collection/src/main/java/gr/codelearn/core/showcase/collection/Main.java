8ab14309cfd08958019418ea5b08666a64b7a083
IoannisKlian <giannisklian@gmail.com>
2022-09-12 16:19:08 +0300

    Initial commit

---
+++ java-path-core-collection/src/main/java/gr/codelearn/core/showcase/collection/Main.java
@@ -0,0 +1,7 @@
+package gr.codelearn.core.showcase.collection;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Collection module examples");
+    }
+}
