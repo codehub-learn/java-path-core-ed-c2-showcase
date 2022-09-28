5b313ae1fd0027448cdf2900187ed7e383061a21
Ioannis Daniil <jondan97@gmail.com>
2022-09-28 10:36:51 +0300

    added department and way to save entity into the database

--- java-path-core-hibernate/src/main/java/gr/codelearn/core/showcase/hibernate/Main.java
+++ java-path-core-hibernate/src/main/java/gr/codelearn/core/showcase/hibernate/Main.java
@@ -1,5 +1,19 @@
 package gr.codelearn.core.showcase.hibernate;
 
-public class Main {
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
 
+import gr.codelearn.core.showcase.hibernate.model.Department;
+
+public class Main {
+	public static void main(String[] args) {
+		EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("UniversityModule");
+		EntityManager entityManager = entityManagerFactory.createEntityManager();
+		Department department = new Department("Computer Science");
+		entityManager.getTransaction().begin();
+		entityManager.persist(department);
+		entityManager.getTransaction().commit();
+		entityManagerFactory.close();
+	}
 }
--- java-path-core-hibernate/src/main/java/gr/codelearn/core/showcase/hibernate/model/Department.java
+++ java-path-core-hibernate/src/main/java/gr/codelearn/core/showcase/hibernate/model/Department.java
@@ -2,9 +2,24 @@ package gr.codelearn.core.showcase.hibernate.model;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
 @Entity
 public class Department {
-	@Column
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private long id;
+	
+	@Column()
 	private String name;
+
+	public Department(String name) {
+		super();
+		this.name = name;
+	}
+	
+	
 }
